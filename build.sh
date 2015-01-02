653f5282eef1c47f0f97fa83f0ebde19848d9617
Dave Lambley <dave@lambley.me.uk>
2015-01-02 00:07:57 +0000

    Clone the git repo.

--- build.sh
+++ build.sh
@@ -4,4 +4,12 @@ mkdir out
 cp background/* out/
 ttree -a -f ttreerc -s src -d out
 chmod a+rX -R out/
+mkdir out/lbwsite.git
+cd out/lbwsite.git
+git init --bare
+cd -
+git push out/lbwsite.git master
+cd out/lbwsite.git
 git update-server-info
+cd -
+chmod a+rX -R out/
